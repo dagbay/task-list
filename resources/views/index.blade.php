ed070ae2e3b63caa4e9e296ce5bb680bfd33e96d
Daniel Agbay <dagbay@outlook.com>
2024-07-23 23:07:12 +1000

    Added required HTML for pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,17 @@
 
 @section('content')
 <div>
-  @if (count($tasks))
-    @foreach ($tasks as $task)
-      <div>
-        <a href="{{ route('tasks.show', ['task' => $task]) }}">{{ $task->title }}</a>
-      </div>
-    @endforeach
-  @else
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['task' => $task]) }}">{{ $task->title }}</a>
+    </div>
+  @empty
     <div>There are no tasks!</div>
+  @endforelse
+  
+  @if ($tasks->count())
+    <nav> {{ $tasks->links('pagination::simple-bootstrap-5') }} </nav>
+    <div> Showing {{ $tasks->firstItem() }} to {{ $tasks->lastItem() }} of {{ $tasks->total() }} results </div> 
   @endif
 </div>
 @endsection
